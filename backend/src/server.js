cbce3b2f094d7bec1d84ee4fd5fea3587e94c1ec
roy wu <136756432+wujinfen@users.noreply.github.com>
2025-07-01 21:28:25 -0400

    setup for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ dotenv.config()
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+import path from "path"
+
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
@@ -11,6 +13,9 @@ import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js"
 
 const app = express()
+
+const __dirname = path.resolve()
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true //allow frontend to send cookies
@@ -22,6 +27,14 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "/index.html"))
+    })
+}
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
